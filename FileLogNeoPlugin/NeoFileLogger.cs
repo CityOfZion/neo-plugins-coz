7a098af7973b57f403ca70e5fc9bb41f091290f2
Shargon <shargon@gmail.com>
2018-02-13 12:04:06 +0100

    First commit

---
+++ FileLogNeoPlugin/NeoFileLogger.cs
@@ -0,0 +1,61 @@
+﻿using Neo.Plugins;
+using System;
+using System.IO;
+using System.Runtime.ExceptionServices;
+
+namespace FileLogNeoPlugin
+{
+    public class NeoFileLogger : NeoLogPlugin
+    {
+        /// <summary>
+        /// File path
+        /// </summary>
+        public string LogFilePath { get; set; }
+
+        public override bool Load()
+        {
+            LogFilePath = Path.Combine(".", "log.txt");
+
+            AppDomain.CurrentDomain.FirstChanceException += CurrentDomain_FirstChanceException;
+            AppDomain.CurrentDomain.UnhandledException += CurrentDomain_UnhandledException;
+
+            return base.Load();
+        }
+        public override void Unload()
+        {
+            AppDomain.CurrentDomain.FirstChanceException -= CurrentDomain_FirstChanceException;
+            AppDomain.CurrentDomain.UnhandledException -= CurrentDomain_UnhandledException;
+
+            base.Unload();
+        }
+
+        void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
+        {
+            Log((Exception)e.ExceptionObject);
+        }
+
+        void CurrentDomain_FirstChanceException(object sender, FirstChanceExceptionEventArgs e)
+        {
+            Log(e.Exception);
+        }
+
+        public override void Log(Exception error)
+        {
+            if (error == null) return;
+
+            Log(error.ToString());
+        }
+
+        public override void Log(string message)
+        {
+            try
+            {
+                lock (this)
+                {
+                    File.AppendAllText(LogFilePath, DateTime.Now.ToString() + " - " + message + Environment.NewLine);
+                }
+            }
+            catch { }
+        }
+    }
+}
---
+++ RpcSampleNeoPlugin/RpcSampleNeoPlugin.cs
@@ -0,0 +1,22 @@
+﻿using Neo.IO.Json;
+using Neo.Plugins;
+
+namespace RpcSampleNeoPlugin
+{
+    public class NeoRpcSample : NeoRpcPlugin
+    {
+        public override JObject RpcCall(NeoRpcPluginArgs args)
+        {
+            if (string.Compare(args.Method, "dummy") == 0)
+            {
+                args.Handle = true;
+
+                JObject ret = new JObject();
+                ret["dummyResult"] = true;
+                return ret;
+            }
+
+            return null;
+        }
+    }
+}
